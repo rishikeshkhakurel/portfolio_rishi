9652d865881e185dad32cdefbce60dcf82f7ca9a
rishikeshkhakurel <khakurel.rishikesh1@gmail.com>
2023-08-03 18:47:35 +0545

    refactor: onclick outside menu close menu

--- src/component/header/index.js
+++ src/component/header/index.js
@@ -2,32 +2,54 @@ import Logo from '../../common/assets/logo.png'
 import { IoMenu } from "react-icons/io5";
 import { AiOutlineClose } from "react-icons/ai";
 import HeaderStyled from './header-style';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 const Header = ({ handleMenuClick }) => {
     const [menu, setMenu] = useState(false);
+
+    const handleMenu = (name) => {
+        setMenu(false);
+        handleMenuClick(name);
+    };
+
+    const handleMenuToggle = (e) => {
+        e.stopPropagation(); // Prevent event propagation to document
+        setMenu((prevMenu) => !prevMenu);
+    };
+
+    const handleClickOutside = () => {
+        setMenu(false);
+    };
+
+    useEffect(() => {
+        document.addEventListener("click", handleClickOutside);
+        return () => {
+            document.removeEventListener("click", handleClickOutside);
+        };
+    }, []);
     return (
         <HeaderStyled >
             <div id="header-margin">
                 <div id="logo">
                     <img alt='Logo' src={Logo} />
                 </div>
-                {menu ? <AiOutlineClose className="menu-icon" onClick={() => setMenu(false)} /> : <IoMenu className="menu-icon" onClick={() => setMenu(true)} />}
+                {menu ? <AiOutlineClose className="menu-icon" onClick={handleMenuToggle} /> : <IoMenu className="menu-icon" onClick={handleMenuToggle} />}
                 <div id="menu">
-                    <p onClick={() => handleMenuClick('about-me')}>About Me</p>
-                    <p onClick={() => handleMenuClick('skills')}>Skills</p>
-                    <p onClick={() => handleMenuClick('portfolio')}>Portfolio</p>
-                    <button onClick={() => handleMenuClick('contact')}>Contact Me</button>
+                    <p onClick={() => handleMenu('about-me')}>About Me</p>
+                    <p onClick={() => handleMenu('skills')}>Skills</p>
+                    <p onClick={() => handleMenu('portfolio')}>Portfolio</p>
+                    <button onClick={() => handleMenu('contact')}>Contact Me</button>
                 </div>
             </div>
             {
-                menu && <div id="menu-responsive">
-                    <p onClick={() => handleMenuClick('about-me')}>About Me</p>
-                    <p onClick={() => handleMenuClick('skills')}>Skills</p>
-                    <p onClick={() => handleMenuClick('portfolio')}>Portfolio</p>
-                    <p onClick={() => handleMenuClick('contact')}>Contact Me</p>
-                </div>
-            }
+                menu && (
+                    <div id="menu-responsive" >
+                        <p onClick={() => handleMenu('about-me')}>About Me</p>
+                        <p onClick={() => handleMenu('skills')}>Skills</p>
+                        <p onClick={() => handleMenu('portfolio')}>Portfolio</p>
+                        <p onClick={() => handleMenu('contact')}>Contact Me</p>
+                    </div>
+                )}
         </HeaderStyled>
     )
 }
